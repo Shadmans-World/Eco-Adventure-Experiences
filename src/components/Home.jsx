a9e53aaa2c3d6b5d8785ee11cf77a27e9a4bcc38
Shadmans-World <shaon10516045@gmail.com>
2024-11-20 01:10:40 +0600

    2 section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Banner from './Banner';
 import CardContainer from './CardContainer';
 
+import Extended from './Extended';
+
 const Home = () => {
     return (
         <div >
            <Banner/>
            <CardContainer></CardContainer>
+           <Extended></Extended>
         </div>
     );
 };
